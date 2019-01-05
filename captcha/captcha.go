c051f9fcebac49aebbfe9ecdc88e832a7cea52d2
Teerapong Singthong <teerapong.s@linecorp.com>
2019-01-05 15:33:07 +0700

    feat: add execption handler

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -6,7 +6,11 @@ func Do(pattern int, leftO int, operator int, rightO int) string {
 	result := ""
 
 	if rightO > 9 {
-		return "righ operand should not greather than 9"
+		return "righ operand should not greater than 9"
+	}
+
+	if operator > 4 {
+		return "operator should not greater than 4"
 	}
 
 	numberWord := [9]string{
